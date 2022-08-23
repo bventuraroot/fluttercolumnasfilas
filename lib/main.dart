8a3d59e4b6bb11aa78d8379b45bd6f10b886c71d
briandagoberto20 <briandagoberto20@hotmail.com>
2022-08-22 18:49:24 -0600

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:fluttercolumnasfilas/constantes/const.dart';
 
@@ -14,7 +16,7 @@ class FilaCol extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "Filas y columnas",
       home: filacol(),
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(primarySwatch: Colors.green),
     );
   }
 }
@@ -61,12 +63,20 @@ Widget fondo() {
   return Container(
     decoration: BoxDecoration(
         image: DecorationImage(
-            fit: BoxFit.fill,
+            opacity: 60,
+            fit: BoxFit.cover,
             image: NetworkImage(
                 "https://nebulae-assets.s3.amazonaws.com/3b56d17152bd46c295797a7eaab1f244.jpg"))),
     child: Center(
       child: Column(
-        children: <Widget>[nombre()],
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          nombre(),
+          registroUsuario(),
+          registroPass(),
+          btnIngresar(),
+          opData()
+        ],
       ),
     ),
   );
@@ -74,5 +84,45 @@ Widget fondo() {
 
 Widget nombre() {
   return Text("Iniciar Sesion",
-      style: TextStyle(color: Colors.blue, fontSize: 30.0));
+      style: TextStyle(color: Colors.blueGrey, fontSize: 30.0));
+}
+
+Widget registroUsuario() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+    child: TextField(
+      decoration: InputDecoration(
+          hintText: "Ingrese su nombre",
+          fillColor: Colors.blueAccent,
+          filled: true),
+    ),
+  );
+}
+
+Widget registroPass() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+    child: TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+          hintText: "Ingrese su Password",
+          fillColor: Colors.blueAccent,
+          filled: true),
+    ),
+  );
+}
+
+Widget btnIngresar() {
+  return ElevatedButton.icon(
+      onPressed: () {},
+      icon: Icon(Icons.enhance_photo_translate),
+      label: Text("Ingresar"));
+}
+
+Widget opData() {
+  return Text("Hola");
+}
+
+OptionElement btnRol() {
+  return OptionElement();
 }
